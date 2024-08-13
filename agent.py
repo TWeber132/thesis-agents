523a816cc701ed47142a2efdb9ac157ded49047a
TWeber132 <till-mathis.weber@web.de>
2024-08-13 07:24:32 +0000

    fixed error calculating functions

--- agent.py
+++ agent.py
@@ -13,44 +13,52 @@ class Agent(ABC):
         ...
 
     def calculate_rot_error(self, gt_quat, quat):
-        conj_gtq = np.array(gt_quat) * np.array([-1.0, -1.0, -1.0, 1.0])
-        # expected to be 1 - unit quaternion
-        norm_gtq = np.sqrt(np.dot(gt_quat, gt_quat))
-        print("norm_gtq: ", norm_gtq)
-        inv_gtq = conj_gtq / norm_gtq
-        diff_q = np.array(quat) * inv_gtq
-
-        # diff_q = p.getDifferenceQuaternion(gt_rot, rot)
-        norm_q = np.sqrt(np.dot(diff_q, diff_q))
-        print("norm_q: ", norm_q)
-        if abs(norm_q - 1) > 0.00005:  # How far from being a unit quaternion
-            diff_q = diff_q / norm_q
+        gt_quat = np.array(gt_quat)
+        quat = np.array(quat) # x y z w
+        inv_q = quat_inverse(quat)
+        diff_q = quat_mult(gt_quat, inv_q)
 
         diff_i = diff_q[:3]
-        norm_i = np.sqrt(np.dot(diff_i, diff_i))
+        norm_i = normalize(diff_i)
         diff_r = diff_q[3]
         return 2 * np.arctan2(norm_i, diff_r)
         # return 2 * np.arccos(diff_r) # Not as stable?
 
     def calculate_trans_error(self, gt_trans, trans):
+        gt_trans = np.array(gt_trans)
+        tranas = np.array(trans)
         diff = gt_trans - trans
-        magn = np.sqrt(np.dot(diff, diff))
+        magn = normalize(diff)
         return magn
 
-    def calculate_errors(self, gt_action, action):
-        t_errors = []
-        r_errors = []
+    def calculate_error(self, gt_pose, pose):
         # Not possible at the moment, because of random nature and the
         # returned poses, that are not validated to actually pick objects
         # gt_action = self.act(None, None)
-        assert len(gt_action) == len(action)
+        
+        trans, rot = pose
+        gt_trans, gt_rot = gt_pose
+        t_error = self.calculate_trans_error(gt_trans, trans)
+        r_error = self.calculate_rot_error(gt_rot, rot)
+        error = [t_error, r_error]
+        return error
+    
+def quat_mult(q1, q2):
+    x1, y1, z1, w1 = q1
+    x2, y2, z2, w2 = q2
+    x = w1*x2 + x1*w2 + y1*z2 - z1*y2
+    y = w1*y2 + y1*w2 + z1*x2 - x1*z2
+    z = w1*z2 + z1*w2 + x1*y2 - y1*x2
+    w = w1*w2 - x1*x2 - y1*y2 - z1*z2
+    return np.array([x, y, z, w])
 
-        for gt_pose, pose in zip(gt_action, action):
-            trans, rot = pose
-            gt_trans, gt_rot = gt_pose
-            t_error = self.calculate_trans_loss(gt_trans, trans)
-            r_error = self.calculate_rot_loss(gt_rot, rot)
-            t_errors.append(t_error)
-            r_errors.append(r_error)
+def quat_inverse(q1):
+    conj = q1 * np.array([-1.0, -1.0, -1.0, 1.0])
+    # expected to be 1 -> unit quaternion
+    norm = normalize(q1)
+    return conj / norm
 
-        return t_errors, r_errors
+def normalize(m1):
+    return np.sqrt(np.dot(m1, m1))
+
+    
